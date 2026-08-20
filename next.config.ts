3182c0723dc1f54ef7c7468d8d3eb4a6263dfde9
syntax-tm <13120514+syntax-tm@users.noreply.github.com>
2026-08-20 17:48:09 -0500

    Added pdf to webpack resources

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,13 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.pdf$/,
+      type: 'asset/resource',
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
